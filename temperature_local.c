b353087e89c49fd4ebdd6757a009a155914193cc
Carlos Moro <carlosmoro95@gmail.com>
2018-11-14 17:29:41 +0100

    Pub bien

--- temperature_local.c
+++ temperature_local.c
@@ -13,20 +13,18 @@
 #include <mosquitto.h>
 #include "common.h"
 
-#define MQTT_BROKER "192.168.1.91"
 #define MQTT_PORT 1883
-#define MQTT_TOPIC "sensors"
 
 struct mosquitto* mosq;
 
 int main(int argc, char* argv[])
 {
 
-	if (argc != 3)
+    if (argc != 3)
     {
         fprintf (stderr, "uso: thermo ip topic\n");
         exit (1);
-	}
+    }
 
     mosquitto_lib_init();
 
@@ -38,15 +36,23 @@ int main(int argc, char* argv[])
     }
 
     float data = 0.f;
-	char ip[16];
-	char topic[50];
+    char ip[16];
+    char topic[50];
     char msg[170];
+
     memset(msg, '\0', 170);
     memset(topic, '\0', 50);
     memset(ip, '\0', 16);
 
     strcpy(ip, argv[1]);
     strcpy(topic, argv[2]);
+
+    int ret = mosquitto_connect(mosq, ip, MQTT_PORT, 0);
+
+    if(ret) {
+        fprintf(stderr, "Can't connect to Mosquitto server\n");
+        exit(1);
+    }
 
     i2c_init();
     i2c_select_bus(MIKROBUS_1);
@@ -55,50 +61,48 @@ int main(int argc, char* argv[])
     thermo3_click_get_temperature(&data);
 
     // Temperatura ambiente Ci40
-    sprintf(msg, "Temperatura ambiente Ci40: %f", ip, MQTT_TOPIC, data);
-    mosquitto_publish(mosq, NULL, MQTT_TOPIC, sizeof(msg), msg, 0, 1);    
-    /*system(command);
+    sprintf(msg, "Temperatura ambiente Ci40: %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Temperatura ambiente SensorTag temperatura
     data = read_tmp(AMB_TMP_VALUE, NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Temperatura ambiente SensorTag(tmp): %f'", ip, topic, data);
-    system(command);
+    sprintf(msg, "Temperatura ambiente SensorTag(tmp): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Temperatura objeto SensorTag temperatura
     data = read_tmp(OBJ_TMP_VALUE, NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Temperatura objeto SensorTag(tmp): %f'", ip, topic, data);
-    system(command);
+    sprintf(msg, "Temperatura objeto SensorTag(tmp): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Temperatura ambiente SensorTag presion
     data = read_bmp(AMB_TMP_VALUE, NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Temperatura ambiente SensorTag(bmp): %f'", ip, topic, data);
-    system(command);
+    sprintf(msg, "Temperatura ambiente SensorTag(bmp): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Presion SensorTag presion
     data = read_bmp(BMP_VALUE, NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Presion SensorTag(bmp): %f'", ip, topic, data);
-    system(command);
+    sprintf(msg, "Presion SensorTag(bmp): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Temperatura ambiente SensorTag humedad
     data = read_hum(AMB_TMP_VALUE, NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Temperatura ambiente SensorTag(hum): %f'", ip, topic, data);
-    system(command);
+    sprintf(msg, "Temperatura ambiente SensorTag(hum): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Humedad SensorTag humedad
     data = read_hum(HUM_VALUE, NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Humedad SensorTag(hum): %f'", ip, topic, data);
-    system(command);
+    sprintf(msg, "Humedad SensorTag(hum): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Sensacion termica SensorTag humedad
     data = read_hum(SEN_TMP_VALUE, NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Sensacion termica SensorTag(hum) %f'", ip, topic, data);
-    system(command);
+    sprintf(msg, "Sensacion termica SensorTag(hum): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);
     sleep(2);
     // Luminosidad SensorTag luminosidad
     data = read_opt(NULL);
-    sprintf(command, "mosquitto_pub -h %s -p 1883 -t '%s' -m 'Luminosidad SensorTag(opt) %f'", ip, topic, data);
-    system(command);*/
-    
+    sprintf(msg, "Luminosidad SensorTag(opt): %f", data);
+    mosquitto_publish(mosq, NULL, topic, sizeof(msg), msg, 0, 1);    
 
     thermo3_click_disable();
 
